5f9c428b86986ed65adc993828e84ffdcdc098c5
AslanMus <asmus01@mail.ru>
2024-05-12 13:12:54 +0300

    Ok

--- src/test/java/pages/TestPageDemoqa.java
+++ src/test/java/pages/TestPageDemoqa.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class TestPageDemoqa {
 
@@ -18,11 +19,14 @@ public class TestPageDemoqa {
             lastNameInput = $("#lastName");
 
     public TestPageDemoqa openPage() {
-        open("https://demoqa.com/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        Selenide.executeJavaScript("$('#RightSide_Advertisement').remove()");//delete ads-banners
-        Selenide.executeJavaScript("$('footer').remove()");//delete footer
-        Selenide.executeJavaScript("$('#fixedban').remove()");//delete ads-banner
+        step("Открытие страницы", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            Selenide.executeJavaScript("$('#RightSide_Advertisement').remove()");//delete ads-banners
+            Selenide.executeJavaScript("$('footer').remove()");//delete footer
+            Selenide.executeJavaScript("$('#fixedban').remove()");//delete ads-banner
+
+        });
         return this;
     }
 
@@ -76,19 +80,19 @@ public class TestPageDemoqa {
         return this;
     }
 
-    public TestPageDemoqa setAddress (String value) {
+    public TestPageDemoqa setAddress(String value) {
         $("#currentAddress").setValue(value);
         return this;
 
     }
 
-    public TestPageDemoqa setState (String value) {
+    public TestPageDemoqa setState(String value) {
         $("#state").click();
         $(byText(value)).click();
         return this;
     }
 
-    public TestPageDemoqa setCity (String value) {
+    public TestPageDemoqa setCity(String value) {
         $("#city").click();
         $(byText(value)).click();
         return this;
@@ -100,6 +104,7 @@ public class TestPageDemoqa {
         return this;
 
     }
+
     public TestPageDemoqa verifyModalWindow() {
         registrationResultsModal.verifyModalAppears();
         return this;
